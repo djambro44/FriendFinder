64b4e13ad805b0b583bdc90ae96b72b841e1e5d2
Dylan Ambrose <djambro44@gmail.com>
2020-05-01 15:07:27 -0700

    added friends.js profiles and working through html routes

---
+++ app/data/friends.js
@@ -0,0 +1,103 @@
+//this will store the baseline friend data
+
+var friendsArray = [
+    {
+        name: "Grant",
+        photo: "https://djambro44.github.io/FriendFinder/app/public/images/grantPic",
+        scores: [
+            1,
+            2,
+            3,
+            2,
+            1,
+            3,
+            2,
+            1,
+            2,
+            3
+        ]
+    },
+    {
+        name: "Miles",
+        photo: "https://djambro44.github.io/FriendFinder/app/public/images/milesPic",
+        scores: [
+            4,
+            3,
+            3,
+            4,
+            5,
+            3,
+            2,
+            5,
+            5,
+            5
+        ]
+    },
+    {
+        name: "Tyler",
+        photo: "https://djambro44.github.io/FriendFinder/app/public/images/tylerPic",
+        scores: [
+            5,
+            2,
+            3,
+            1,
+            1,
+            4,
+            2,
+            2,
+            2,
+            5
+        ]
+    },
+    {
+        name: "Connor",
+        photo: "https://djambro44.github.io/FriendFinder/app/public/images/connorPic",
+        scores: [
+            2,
+            3,
+            1,
+            3,
+            5,
+            5,
+            2,
+            1,
+            2,
+            3
+        ]
+    },
+    {
+        name: "Victor",
+        photo: "https://djambro44.github.io/FriendFinder/app/public/images/vicPic",
+        scores: [
+            5,
+            2,
+            3,
+            2,
+            1,
+            3,
+            2,
+            1,
+            2,
+            5
+        ]
+    },
+    {
+        name: "Ryan",
+        photo: "https://djambro44.github.io/FriendFinder/app/public/images/rynePic",
+        scores: [
+            1,
+            2,
+            1,
+            2,
+            1,
+            1,
+            2,
+            1,
+            2,
+            1
+        ]
+    }
+
+];
+
+module.exports = friendsArray;
